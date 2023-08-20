58463167a86fcba96ca79e944d02f075bfc8e6ee
Damien <damien.heimes@physik.uni-marburg.de>
2023-08-20 17:16:28 +0200

    Add more functions to test in a new file

---
+++ test_functions_2.py
@@ -0,0 +1,15 @@
+def subtract(x, y):
+    return x - y
+
+def test_subtract():
+    assert subtract(1, 2) == -1
+
+    assert subtract(5, 5) == 0
+
+def divide(x, y):
+    return x / y
+
+def test_multiply():
+    assert divide(10, 5) == 2
+
+    assert divide(5, 5) == 1
