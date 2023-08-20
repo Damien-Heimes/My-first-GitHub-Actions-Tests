1d3a6fda23b9a21556f7a81a6cf8f70aad2b8044
Damien <damien.heimes@physik.uni-marburg.de>
2023-08-20 17:08:31 +0200

    Update workflow and add python file including tests

---
+++ test_functions.py
@@ -0,0 +1,15 @@
+def add_up(x, y):
+    return x + y
+
+def test_add_up():
+    assert add_up(1, 2) == 3
+
+    assert add_up(5, 5) == 10
+
+def multiply(x, y):
+    return x * y
+
+def test_multiply():
+    assert multiply(1, 2) == 2
+
+    assert multiply(5, 5) == 25
